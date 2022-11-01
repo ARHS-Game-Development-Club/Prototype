6f1026d90e62f5abda29a67767228b2fb596475b
Axel S <axelthescratcher@gmail.com>
2022-10-31 20:57:39 -0400

    Added some new assets and changed resolution

--- CharacterMovement.gd
+++ CharacterMovement.gd
@@ -4,7 +4,7 @@ extends KinematicBody2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-export var speed := 700
+export var speed := 150
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
